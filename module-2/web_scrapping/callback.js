bddd59821b459d30f384a06dab5df3251ab4008a
ayush18091998 <ayush41557@gmail.com>
2022-03-26 23:23:25 +0530

    content added

---
+++ module-2/web_scrapping/callback.js
@@ -0,0 +1,17 @@
+const fs= require("fs")
+console.log("Before");
+
+fs.readFile("file.txt",cb)
+function cb(error, content )
+{
+    if(error) 
+     console.log(error)
+
+    else 
+     console.log(content+" ")
+}
+console.log("After")
+console.log("After")
+console.log("After")
+console.log("After")
+console.log("After")
