01547eca15ecc22720cd7269d84bde408bb00fc4
ayush18091998 <ayush41557@gmail.com>
2022-03-27 14:34:10 +0530

    function.js  modified

--- module-2/javascript/function.js
+++ module-2/javascript/function.js
@@ -11,7 +11,7 @@ function add(num1,num2)
    let addition= num1+num2;
    console.log("Addition of numbers is:"+ (num1+num2));
 }
-add(9,4);
+add(9,8);
 
 // function with return and parameter
 function multiply(num1,num2)
@@ -20,10 +20,19 @@ function multiply(num1,num2)
 }
 console.log(multiply(2,3));
 
-// function is in Js are first class citizen
+// function  in Js are first class citizen
 let a=function (num1,num2){
     return num1-num2;
 }
 console.log(a(40,20));
 
-// iffe
+// immediate invoke function
+
+(function(){
+    console.log("hello from IIFE");
+})();
+
+// IIFE with parameters
+(function(num1,num2){
+    console.log(num1/num2);
+})(10,2);
